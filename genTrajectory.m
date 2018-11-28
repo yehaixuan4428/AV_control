781737508fc81412552a6f0cd5fdfc0e85112b96
sxtywyc <45015830+sxtywyc@users.noreply.github.com>
2018-11-27 22:21:20 -0500

    Create genTrajectory.m

---
+++ genTrajectory.m
@@ -0,0 +1,7 @@
+% this is a trajectory generation script to directly generate reference
+% trajectory from centerline of the road regardless of the resitriction of
+% the left and right sides of the road
+% for obstacles, this function use rrt to generate ramp to avoid collision
+function traj = genTrajectory
+%% gennerate trajectory without obstacles
+
