db40754ac51214c74e9745660c0d4b12e43b2172
sxtywyc <45015830+sxtywyc@users.noreply.github.com>
2018-11-27 22:33:28 -0500

    Update genTrajectory.m

--- genTrajectory.m
+++ genTrajectory.m
@@ -2,6 +2,7 @@
 % trajectory from centerline of the road regardless of the resitriction of
 % the left and right sides of the road
 % for obstacles, this function use rrt to generate ramp to avoid collision
+% traj(1:3,:) = x;y;psi
 function traj = genTrajectory
 %% gennerate trajectory without obstacles
 
