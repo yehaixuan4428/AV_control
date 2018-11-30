34868c60ea01178390312d052bda5398f0cd3894
sxtywyc <45015830+sxtywyc@users.noreply.github.com>
2018-11-30 15:49:36 -0500

    new

--- genTrajectory.m
+++ genTrajectory.m
@@ -3,13 +3,13 @@
 % the left and right sides of the road
 % for obstacles, this function use rrt to generate ramp to avoid collision
 % traj(1:3,:) = x;y;psi
-function traj = genTrajectory(TestTrack,Obs)
+function [Traj_ref_x, Traj_ref_y, Traj_ref_psi] = genTrajectory(cline,theta)
 Obs_default = NaN;
 if nargin == 1
     Obs = Obs_default;
 end
 %% gennerate trajectory without obstacles
-traj(1,:) = TestTrack.cline(1,:);
-traj(2,:) = TestTrack.cline(2,:);
-traj(3,:) = TestTrack.theta;
+Traj_ref_x = cline(1,:);
+Traj_ref_y = cline(2,:);
+Traj_ref_psi = theta;
 end
--- main_control.m
+++ main_control.m
@@ -4,5 +4,25 @@
 %number of time steps between two trajectory points are dynamically
 %determined
 load TestTrack.mat
-[Traj_ref_x, Traj_ref_y, Traj_ref_psi] = genTrajectory(TestTrack);
+load Pcontroller.mat
+[Traj_ref_x, Traj_ref_y, Traj_ref_psi] = genTrajectory(TestTrack.cline,TestTrack.theta);
 timestep = 0.1;
+% define initial state
+z(:,1) = [287;5;-176;0;2;0];
+u(:,1) = Pcontroller*([Traj_ref_x(1);Traj_ref_y(1);Traj_ref_psi(1)]-...
+        [z(1,1);z(3,1);z(5,1)]);
+for i = 2:length(Traj_ref_x)
+    while ~pt_threshold([z(1,end);z(2,end)],[Traj_ref_x(i);Traj_ref_y(i)],[Traj_ref_x(i-1);Traj_ref_y(i-1)])
+        z(:,length(z)+1) = z(:,end)+timestep*vehicle_model(z(:,end),u(:,end));
+    end
+end
+
+function k = pt_threshold(x,x_d,x_d_b)
+dist = norm(x_d-x);
+dist_d = 0.025*norm(x_d-x_d_b);
+    if dist< dist_d
+        k = true;
+    else
+        k = false;
+    end
+end
