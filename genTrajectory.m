58ef583de85fbe9929e9e7c9873ab05bcd1ce205
sxtywyc <45015830+sxtywyc@users.noreply.github.com>
2018-11-29 20:03:43 -0500

    jjj

--- genTrajectory.m
+++ genTrajectory.m
@@ -3,6 +3,13 @@
 % the left and right sides of the road
 % for obstacles, this function use rrt to generate ramp to avoid collision
 % traj(1:3,:) = x;y;psi
-function traj = genTrajectory
+function traj = genTrajectory(TestTrack,Obs)
+Obs_default = NaN;
+if nargin == 1
+    Obs = Obs_default;
+end
 %% gennerate trajectory without obstacles
-
+traj(1,:) = TestTrack.cline(1,:);
+traj(2,:) = TestTrack.cline(2,:);
+traj(3,:) = TestTrack.theta;
+end
---
+++ main_control.m
@@ -0,0 +1,8 @@
+%this function is designed as a cntroller for vehicle to perform feedback
+%control alng the generated trjectory (with or whout obstacles)
+%time step is 0.1s
+%number of time steps between two trajectory points are dynamically
+%determined
+function main_control(TestTrack)
+
+end
