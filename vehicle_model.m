16079c14b2a80f90564ec72b9e619421bcae1afd
sxtywyc <45015830+sxtywyc@users.noreply.github.com>
2018-12-01 17:47:39 -0500

    Revert "gg"
    This reverts commit f64b9c204c125cc1e333f2f91eb3a1e1381a34ff.

--- vehicle_model.m
+++ vehicle_model.m
@@ -31,10 +31,10 @@ Fzr = a/(a+b)*m*g;
 Fmax = 0.7*m*g;
 
 %second level calculation
-alphaf = rad2deg(delta_f - atan((v+a*r)/u));
-alphar = rad2deg(-atan((v-b*r)/u));
-phiyf = (1-Ey)*(alphaf+Shy)+(Ey/By)*atan(By*(alphaf+Shy));
-phiyr = (1-Ey)*(alphar+Shy)+(Ey/By)*atan(By*(alphar+Shy));
+alphaf = delta_f - atan((v+a*r)/u);
+alphar = -atan((v-b*r)/u);
+phiyf = (1-Ey)*(alphaf+Shy)+Ey/By*atan(By*(alphaf+Shy));
+phiyr = (1-Ey)*(alphar+Shy)+Ey/By*atan(By*(alphar+Shy));
 Fyf = Fzf*Dy*sin(Cy*atan(By*phiyf))+Svy;
 Fyr = Fzr*Dy*sin(Cy*atan(By*phiyr))+Svy;
 
@@ -47,8 +47,8 @@ end
 
 %dx calculation
 dx = [u*cos(psi)-v*sin(psi);...
-    (1/m)*(-f*m*g+Nw*Fx-Fyf*sin(delta_f))+v*r;...
+    1/m*(-f*m*g+Nw*Fx-Fyf*sin(delta_f))+v*r;...
     u*sin(psi)+v*cos(psi);...
-    (1/m)*(Fyf*cos(delta_f)+Fyr)-u*r;...
+    1/m*(Fyf*cos(delta_f)+Fyr)-u*r;...
     r;...
-    (1/Iz)*(a*Fyf*cos(delta_f)-b*Fyr)];
+    1/Iz*(a*Fyf*cos(delta_f)-b*Fyr)];
